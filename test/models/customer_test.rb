ff195f2e1298b4e8aa76f03d688ab98366e19322
Dianna Tingg <dtingg@gmail.com>
2019-11-05 14:30:08 -0800

    added validation test for customer and customer with no name

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -2,13 +2,18 @@ require "test_helper"
 
 describe Customer do
   describe "validations" do
-    
     it "is valid for a customer with a name, registered_at, postal_code, and phone" do
+      customer = Customer.new(name: "Fred Flintstone", registered_at: DateTime.now, postal_code: "98104", phone: "1234567890")
       
+      expect(customer.valid?).must_equal true
     end
     
     it "is invalid for a customer without a name" do
+      customer = Customer.new(name: nil, registered_at: DateTime.now, postal_code: "98104", phone: "1234567890")
       
+      expect(customer.valid?).must_equal false
+      expect(customer.errors.messages).must_include :name
+      expect(customer.errors.messages[:name]).must_equal ["can't be blank"]
     end
     
     it "is invalid for a customer without a registered_at" do
