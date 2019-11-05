b59354af37f8c7e85fd10461fa6e38eec2e4c728
Dianna Tingg <dtingg@gmail.com>
2019-11-05 14:52:28 -0800

    Added tests for movie model validations

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,5 @@
 class CustomersController < ApplicationController
+  def zomg
+    render json: { test: "it works!"}, status: :ok
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe Movie do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  
+  describe "validations" do
+    it "is valid for a movie with a title, release_date, overview, and inventory" do
+      movie = Movie.new(title: "The Ada Escape", release_date: Date.today, overview: "A thrilling caper", inventory: 12)
+      expect(movie.valid?).must_equal true
+    end
+    
+    it "is invalid for a movie without a title" do
+      movie = Movie.new(title: nil, release_date: Date.today, overview: "A thrilling caper", inventory: 12)
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :title
+      expect(movie.errors.messages[:title]).must_equal ["can't be blank"]
+    end
+    
+    it "is invalid for a movie without a release date" do
+      movie = Movie.new(title: "The Ada Escape", release_date: nil, overview: "A thrilling caper", inventory: 12)
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :release_date
+      expect(movie.errors.messages[:release_date]).must_equal ["can't be blank"]
+    end
+    
+    it "is invalid for a movie without an overview" do
+      movie = Movie.new(title: "The Ada Escape", release_date: Date.today, overview: nil, inventory: 12)
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :overview
+      expect(movie.errors.messages[:overview]).must_equal ["can't be blank"]
+    end
+    
+    it "is invalid for a movie without an inventory" do
+      movie = Movie.new(title: "The Ada Escape", release_date: Date.today, overview: "A thrilling caper", inventory: nil)
+      expect(movie.valid?).must_equal false
+      expect(movie.errors.messages).must_include :inventory
+      expect(movie.errors.messages[:inventory]).must_equal ["can't be blank"]
+    end
+  end
 end
