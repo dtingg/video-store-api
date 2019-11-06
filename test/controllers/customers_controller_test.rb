fa886ccbcfd4ae6027add45309a020906d247eb5
Dianna Tingg <dtingg@gmail.com>
2019-11-06 13:19:49 -0800

    added check response to test helper

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -5,33 +5,29 @@ describe CustomersController do
     it "responds with JSON and success" do
       get customers_path
       
-      expect(response.header['Content-Type']).must_include 'json'
-      must_respond_with :ok
+      check_response(expected_type: Array)
     end
     
     it "responds with an array of Customer hashes" do
       get customers_path
       
-      body = JSON.parse(response.body)
+      body = check_response(expected_type: Array)
       
-      expect(body).must_be_instance_of Array
       expect(body.count).must_equal Customer.count
       body.each do |customer|
         expect(customer).must_be_instance_of Hash
         expect(customer.keys.sort).must_equal ["id", "movies_checked_out_count", "name", "phone", "postal_code", "registered_at"]
       end
-      must_respond_with :ok
     end
     
     it "responds with an empty array when there are no customers" do
       Customer.destroy_all
       
       get customers_path
-      body = JSON.parse(response.body)
       
-      expect(body).must_be_instance_of Array
+      body = check_response(expected_type: Array)
+      
       expect(body).must_equal []
-      must_respond_with :ok
     end
   end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -5,33 +5,29 @@ describe MoviesController do
     it "responds with JSON and success" do
       get movies_path
       
-      expect(response.header['Content-Type']).must_include 'json'
-      must_respond_with :ok
+      check_response(expected_type: Array)
     end
     
     it "responds with an array of Movie hashes" do
       get movies_path
       
-      body = JSON.parse(response.body)
+      body = check_response(expected_type: Array)
       
-      expect(body).must_be_instance_of Array
       expect(body.count).must_equal Movie.count
       body.each do |movie|
         expect(movie).must_be_instance_of Hash
         expect(movie.keys.sort).must_equal ["id", "release_date", "title"]
       end
-      must_respond_with :ok
     end
     
     it "responds with an empty array when there are no movie" do
       Movie.destroy_all
       
       get movies_path
-      body = JSON.parse(response.body)
       
-      expect(body).must_be_instance_of Array
+      body = check_response(expected_type: Array)
+      
       expect(body).must_equal []
-      must_respond_with :ok
     end
   end
   
@@ -39,7 +35,7 @@ describe MoviesController do
     it "responds with success for a valid movie" do
     end
     
-    it "responds with not_found for an invalid movie" do
+    it "responds with bad_request for an invalid movie" do
     end
   end
 end
