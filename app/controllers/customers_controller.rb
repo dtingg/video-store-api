7aa500daad0b8a1ac866d3a1a8b7e33063775713
Dianna Tingg <dtingg@gmail.com>
2019-11-08 16:05:10 -0800

    fixed test for sort

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,21 @@ class CustomersController < ApplicationController
   def index
     customer_json = Customer.update_customer_json
     
+    if params[:sort] == "name" || params[:sort] == "postal_code" || params[:sort] == "registered_at"
+      customer_json = customer_json.sort_by { |hash| hash[params[:sort]] }
+    end
+    
+    if params[:n] && params[:p] == nil
+      n = params[:n].to_i
+      customer_json = customer_json[0..n]
+    elsif params[:n] && params[:p]
+      n = params[:n].to_i
+      p = params[:p].to_i
+      starting = n * ( p - 1 )
+      ending = (n * p) - 1
+      customer_json = customer_json[starting..ending]
+    end
+    
     render json: customer_json, status: :ok  
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -29,5 +29,17 @@ describe CustomersController do
       
       expect(body).must_equal []
     end
+    
+    it "responds with JSON and success for sorting by name with number of responses and pages" do
+      get customers_path, params: {sort: "name", n: 5, p: 2}
+      
+      body = check_response(expected_type: Array)
+      
+      expect(body.count).must_equal 5
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal ["id", "movies_checked_out_count", "name", "phone", "postal_code", "registered_at"]
+      end
+    end
   end
 end
